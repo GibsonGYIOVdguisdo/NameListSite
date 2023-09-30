6a6a54fef6d84060c26053722fe086ba1db349c8
Gibson <114831858+GibsonGYIOVdguisdo@users.noreply.github.com>
2023-09-30 20:13:56 +0100

    Began improving usability to site
    Made the open file button work
    Changed colour of many elements
    Added hover and click effects to the buttons
    Moved remove pupil button to end of row

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 console.log("Loaded");
 
-const reader = new FileReader();
+const reader1 = new FileReader();
+const reader2 = new FileReader();
 const uploadButton = document.getElementById("addToListButton");
 const fileInput = document.getElementById("fileUpload");
 const nameInput = document.getElementById("nameInput");
 const addNameButton = document.getElementById("nameConfirm");
 const nameArea = document.getElementById("nameArea");
 const downloadButton = document.getElementById("downloadA");
-
+const openButton = document.getElementById("openToListButton");
 let nameCount = 0;
 let allPupils = {};
 uploadButton.onclick = () => {
     console.log("File read");
     if (fileInput.files[0] != undefined){
-        reader.readAsText(fileInput.files[0]);
+        reader1.readAsText(fileInput.files[0]);
     }
 }
 
@@ -49,9 +50,11 @@ function addPupilName(pupilName){
     let tempBut = document.createElement("button");
     tempPar.innerHTML = pupilName;
     tempBut.innerHTML = "remove";
+    tempBut.className = "pupilRemoveButton";
     tempPar.className = "pupilName";
     tempDiv.appendChild(tempPar);
     tempDiv.appendChild(tempBut);
+    tempDiv.className = "pupilDiv";
     nameArea.appendChild(tempDiv);
     tempBut.id = "pupil"+nameCount;
     tempBut.onclick = () => {
@@ -65,15 +68,39 @@ function addPupilName(pupilName){
     updateDownloadButton();
 }
 
-addNameButton.onclick = () => {
-    console.log("sd");
+
+function addNameViaInputField(){
     if (nameInput.value){
         addPupilName(nameInput.value);
     }
+    nameInput.value = "";
 }
+nameInput.addEventListener("keyup", (event) => {
+    if(event.key == "Enter"){
+        addNameViaInputField();
+    }
+    nameInput:focus = false;
+})
 
 
-reader.onload = (result) =>{
+addNameButton.onclick = addNameViaInputField;
+
+openButton.onclick = () =>{
+    reader2.readAsText(fileInput.files[0]);
+
+}
+reader1.onload = (result) =>{
+    let allText = result.target.result;
+    allText = allText.replaceAll("\r","");
+    let fileAsArray = allText.split("\n");
+    for (let pupil of fileAsArray){
+        addPupilName(pupil);
+    }
+}
+reader2.onload = (result) =>{
+    console.log("sad"); 
+    allPupils = {};
+    nameArea.innerHTML="";
     let allText = result.target.result;
     allText = allText.replaceAll("\r","");
     let fileAsArray = allText.split("\n");
